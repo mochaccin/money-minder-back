e036c92d5b336b7e4d9222cbd1362dd20473b05a
“mochaccin” <mochaccindotcl@gmail.com>
2024-08-14 18:21:19 -0400

    handlers for updating password and name for the users

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -22,7 +22,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) error {
 	if derr != nil {
 		return APIError{
 			Status: http.StatusBadRequest,
-			Msg:    "Couldnt create user, verify that the values are formatted corrected",
+			Msg:    "Couldnt create user, verify that the values are formatted correctly",
 		}
 	}
 
@@ -35,4 +35,58 @@ func CreateUser(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	return WriteJSON(w, http.StatusOK, result)
+}
+
+func UpdateUserPassword(w http.ResponseWriter, r *http.Request) error {
+	updateUsr := &UpdateUserPasswordRequest{}
+	derr := json.NewDecoder(r.Body).Decode(updateUsr)
+
+	if derr != nil {
+		return APIError{
+			Status: http.StatusBadRequest,
+			Msg:    "Couldnt update user password, verify that the values are formatted correctly",
+		}
+	}
+
+	err := userRepository.UpdatePassword(updateUsr.userId, updateUsr.newPassword)
+	if err != nil {
+		return APIError{
+			Status: http.StatusInternalServerError,
+			Msg:    err.Error(),
+		}
+	}
+
+	return WriteJSON(w, http.StatusOK, "User's password updated succesfully")
+}
+
+func UpdateUserName(w http.ResponseWriter, r *http.Request) error {
+	updateUsr := &UpdateUserNameRequest{}
+	derr := json.NewDecoder(r.Body).Decode(updateUsr)
+
+	if derr != nil {
+		return APIError{
+			Status: http.StatusBadRequest,
+			Msg:    "Couldnt update user username, verify that the values are formatted correctly",
+		}
+	}
+
+	err := userRepository.UpdateName(updateUsr.userId, updateUsr.newUsername)
+	if err != nil {
+		return APIError{
+			Status: http.StatusInternalServerError,
+			Msg:    err.Error(),
+		}
+	}
+
+	return WriteJSON(w, http.StatusOK, "User's username updated succesfully")
+}
+
+type UpdateUserPasswordRequest struct {
+	userId      string
+	newPassword string
+}
+
+type UpdateUserNameRequest struct {
+	userId      string
+	newUsername string
 }
